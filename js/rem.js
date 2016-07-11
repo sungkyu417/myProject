9e53621cdb5eb3d6ad1044fd31602641aa967e44
sungkyu417 <1137763625@qq.com>
2016-07-11 09:50:55 +0800

    test

---
+++ js/rem.js
@@ -0,0 +1,15 @@
+//页面初始化时自动获取font-size,绑定resize事件 for rem自适应
+(function (doc, win) {
+          var docEl = doc.documentElement,
+            resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+            recalc = function () {
+              var clientWidth = docEl.clientWidth;
+              if (!clientWidth) return;
+              docEl.style.fontSize = 100 * (clientWidth / 320) + 'px';
+            };
+
+          if (!doc.addEventListener) return;
+          win.addEventListener(resizeEvt, recalc, false);
+          doc.addEventListener('DOMContentLoaded', recalc, false);
+        })(document, window);
+
